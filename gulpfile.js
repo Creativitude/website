cc257f8e677625d018061124c2861f0272e6954c
LENEX\naveen <naveen.cooray.nc@gmail.com>
2018-02-19 14:42:18 +0530

    setting up project with gulp

---
+++ gulpfile.js
@@ -0,0 +1,181 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+
+
+/*
+   --- Top Level Functions ---
+
+    gulp.task   -   define tasks
+    gulp.src    -   Point to files to use
+    gulp.dest   -   Points to folder to output
+    gulp.watch  -   Watch files and folders for changes
+
+*/
+
+// Copy all html files
+gulp.task('copyHtml', function() {
+    gulp.src('src/*.html')
+        .pipe(gulp.dest('dist'));
+});
+
+
+// minify and concat all custom js files
+gulp.task('scripts', function() {
+    gulp.src('src/js/custom/*.js')
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+// minify and concat all custom js files
+gulp.task('css', function() {
+    gulp.src('src/css/custom/*.css')
+        .pipe(concat('main.css'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/css'));
+});
+
+//optimize and copy bg images to production/images
+gulp.task('bgImage', () =>
+	gulp.src('src/images/bg/*')
+    .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.optipng({optimizationLevel: 5}),
+        imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}
+            ]
+        })
+    ]))
+	.pipe(gulp.dest('dist/images'))
+);
+
+
+//optimize and copy logo images to production/images
+gulp.task('logo', () =>
+	gulp.src('src/images/logo/*')
+    .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.optipng({optimizationLevel: 5}),
+        imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}
+            ]
+        })
+    ]))
+	.pipe(gulp.dest('dist/images'))
+);
+
+
+//optimize and copy overlays images to production/images
+gulp.task('overlays', () =>
+	gulp.src('src/images/overlays/*')
+    .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.optipng({optimizationLevel: 5}),
+        imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}
+            ]
+        })
+    ]))
+	.pipe(gulp.dest('dist/images'))
+);
+
+
+//copy portfolio images to production/images
+gulp.task('portfolio', () =>
+	gulp.src('src/images/portfolio/*')
+    .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.optipng({optimizationLevel: 5}),
+        imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}
+            ]
+        })
+    ]))
+	.pipe(gulp.dest('dist/images'))
+);
+
+
+//optimize and copy prettyPhoto images to production/images
+gulp.task('prettyPhoto', () =>
+	gulp.src('src/images/prettyPhoto/*')
+    .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.optipng({optimizationLevel: 5}),
+        imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}
+            ]
+        })
+    ]))
+	.pipe(gulp.dest('dist/images'))
+);
+
+
+//optimize and copy team images to production/images
+gulp.task('team', () =>
+	gulp.src('src/images/team/*')
+    .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.optipng({optimizationLevel: 5}),
+        imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}
+            ]
+        })
+    ]))
+	.pipe(gulp.dest('dist/images'))
+);
+
+
+//optimize and copy images to production/images
+gulp.task('img', () =>
+	gulp.src('src/images/*')
+    .pipe(imagemin([
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.jpegtran({progressive: true}),
+        imagemin.optipng({optimizationLevel: 5}),
+        imagemin.svgo({
+            plugins: [
+                {removeViewBox: true},
+                {cleanupIDs: false}
+            ]
+        })
+    ]))
+	.pipe(gulp.dest('dist/images'))
+);
+
+
+//watch for the above functions when files are saved
+gulp.task('watch', function() {
+    gulp.watch('src/js/custom/*.js', ['scripts']);
+    gulp.watch('src/css/custom/*.css', ['css']);
+    gulp.watch('src/images/bg/*', ['bgImage']);
+    gulp.watch('src/images/logo/*', ['logo']);
+    gulp.watch('src/images/overlays/*', ['overlays']);
+    gulp.watch('src/images/portfolio/*', ['portfolio']);
+    gulp.watch('src/images/team/*', ['team']);
+    gulp.watch('src/images/prettyPhoto/*', ['prettyPhoto']);
+    gulp.watch('src/images/*', ['img']);
+    gulp.watch('src/*.html', ['copyHtml']);
+});
+
+
+gulp.task('default', ['scripts','css','bgImage','logo','overlays','portfolio','team','prettyPhoto','img','copyHtml']);
